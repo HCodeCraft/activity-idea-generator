0adf156cd61b1592f4c0be54b715bff2a1c15d9b
Hanna <marmalademarmoset@gmail.com>
2022-12-13 12:45:31 -0800

    Deleting Search bar and Adding a form to add activities to the JSON

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const form = document.getElementById('form')
 form.addEventListener("submit", (event) => {
 event.preventDefault()
 
+let actList;
 
 fetch("db.json")
 .then(res => res.json())
@@ -13,13 +14,12 @@ fetch("db.json")
     // console.log(data.activities[0])
     // console.log(data.activities[0].activity)
 
+    actList = data.activities
+
 let resultBox = document.getElementsByClassName('card-box')
 console.log("resultBox is working", resultBox)
 
-let actList = data.activites
 
-console.log("This is data.activities", data.activities)
-console.log("This is actList", actList)
 
 let input = event.target[0].value
 console.log("Input is working", input)
@@ -27,47 +27,10 @@ console.log("Input is working", input)
 const list = document.getElementById('list')
 console.log("list is working", list)
 
-function setList(results){
-
-    for (const activity of results){
-        // creating a li element for each result item
-        const resultItem = document.createElement('li')
-
-        // adding a class to each item of the results
-        resultItem.classList.add('result-item')
-
-        // grabbing the name of the current point of the loop and adding the name as the list item's text
-        const text = document.createTextNode(actList[activity])
-
-        // appending the text to the result item
-        resultItem.appendChild(text)
-
-        // appending the result item to the list
-        list.appendChild(resultItem)
-    }
-}
-
-console.log("actList is working", actList)
 
 
 
-if (input && input.trim().length >0){
 
-    value = input.trim().toLowerCase()
-
-    setList(actList.filter(activity => {
-        return activity.includes(value)}))
-
-        console.log("The if/else statement is working")
-
-
-} else {
-
-    console.log("No results found :(")
-}
-
-
-})
 
 })
 
