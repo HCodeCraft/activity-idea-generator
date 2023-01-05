f082262fd4f245c2c1f3cabc8db485c0742fadc7
Hanna <marmalademarmoset@gmail.com>
2023-01-05 12:55:35 -0800

    Repairing functionality of Shopping and Medium dropdown, removing console logs

--- script.js
+++ script.js
@@ -38,27 +38,6 @@ let actList;
 
 let result;
 
-// Aquantince help code
-
-// const addObject = async () => {
-//     await fetch("http://localhost:3000/add", {
-//       method: "POST",
-//       headers: {
-//         Accept: "application/json",
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({ didit: "work?" }),
-//     });
-//   };
-
-
-
-
-
-
-
-
-
 form.addEventListener("submit", (event) => {
 event.preventDefault()
 
@@ -75,7 +54,6 @@ fetch('db.json')
         participants: [participants.value],
         price: [price.value],
         link: event.target.link.value
-        // index: (activities.legnth)+1
        
     }
     const addObject = () => {
@@ -119,7 +97,6 @@ function buildTable(results){
         let row = document.createElement('tr');
         console.log("Object Values for results", Object.values(results))
 
-        // let stringResult = JSON.stringify(results)
 
       
         Object.values(result).forEach(text => {
@@ -152,11 +129,8 @@ document.addEventListener("DOMContentLoaded", (e) => {
     
 
    let actList = data.activities
-    console.log("actList", actList)
 
-    console.log("actList[0].activity", actList[0].activity)
 
-   // Random
    randomBtn.addEventListener("click" , (event) => {
 
     let rndmNum = actList[actList.length * Math.random() | 0] 
@@ -174,7 +148,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
     table.appendChild(headerRow);
 
-    let row = document.createElement('tr'); // need to make one for each field?
+    let row = document.createElement('tr'); 
       
 
     
@@ -201,248 +175,198 @@ myTable.appendChild(table);
 
 
 
-    // 1 person
+
 
 onePersonLink.addEventListener("click", (event) => {
 console.log("onePersonLink", onePersonLink)
 let oneResult = actList.filter(activity => activity.participants == "1");
 
-console.log("result", oneResult) // Working
 
-buildTable(oneResult) // not working
+buildTable(oneResult) 
 
 })
 
-//2 people
 
-console.log("twoPeopleLink",twoPeopleLink)
-console.log("actList", actList)
 twoPeopleLink.addEventListener("click", (event) => {
 
 let twoResult = actList.filter(activity => activity.participants[0] == "2" || activity.participants[1] == "2");
 
-console.log("twoResult", twoResult) // Working
 
-buildTable(twoResult) // not working
+buildTable(twoResult) 
 
 })
-// 3 people
+
 
 threeLink.addEventListener("click", (event) => {
 console.log("threeLink", threeLink)
 let threeResult = actList.filter(activity => activity.participants[0] == "3" || activity.participants[1] == "3" || activity.participants[2]== "3");
 
-console.log("threeResult", threeResult) // Working
 
-buildTable(threeResult) // not working
+buildTable(threeResult) 
 
 })
 
-// Relaxation
+
 
 relLink.addEventListener("click", (event) => {
 console.log(event)
 console.log("relLink", relLink)
 let relResult = actList.filter(activity => activity.type[0] == "Relaxation" || activity.type[1]== "Relaxation");
 
-console.log("result", relResult) // Working
 
-buildTable(relResult) // not working
+buildTable(relResult) 
 
 })
 
-// Excersize
+
 
 
 exLink.addEventListener("click", (event) => {
 console.log("exLink", exLink)
 let exResult = actList.filter(activity => activity.type[0] == "Excersize" || activity.type[1] == "Excersize");
 
-console.log("exResult", exResult) // Working
 
-buildTable(exResult) // not working
+buildTable(exResult) 
 
 })
 
-// Art
+
 
 
 artLink.addEventListener("click", (event) => {
 console.log("artLink", artLink)
 let artResult = actList.filter(activity => activity.type[0]== "Art" || activity.type[1] == "Art");
 
-console.log("artResult", artResult) // Working
 
-buildTable(artResult) // not working
+buildTable(artResult) 
 
 })
 
-// Learning
+
 
 
 leaLink.addEventListener("click", (event) => {
 console.log("leaLink", leaLink)
 let leaResult = actList.filter(activity => activity.type[0] == "Learning" || activity.type[1] == "Learning");
 
-console.log("leaResult", leaResult) // Working
 
-buildTable(leaResult) // not working
+buildTable(leaResult) 
 
 })
 
-// Entertainment
+
 
 entLink.addEventListener("click", (event) => {
 console.log("entLink", entLink)
 let entResult = actList.filter(activity => activity.type[0] == "Entertainment" || activity.type[1] == "Entertainment");
 
-console.log("entResult", entResult) // Working
 
-buildTable(entResult) // not working
+buildTable(entResult) 
 
 })
 
-// Organizing 
 
 orgLink.addEventListener("click", (event) => {
 console.log("orgLink", orgLink)
 let orgResult = actList.filter(activity => activity.type[0] == "Organizing" || activity.type[1] == "Organizing");
 
-console.log("orgResult", orgResult) // Working
 
-buildTable(orgResult) // not working
+buildTable(orgResult) 
 
 })
 
-// Food
+
 
 
 foodLink.addEventListener("click", (event) => {
 console.log("foodLink", foodLink)
 let foodResult = actList.filter(activity => activity.type[0] == "Food" || activity.type[1] == "Food");
 
-console.log("foodResult", foodResult) // Working
 
-buildTable(foodResult) // not working
+buildTable(foodResult) 
 
 })
 
-// Shopping
+
 
 shopLink.addEventListener("click", (event) => {
 console.log("shopLink", shopLink)
-let Result = actList.filter(activity => activity.type[0] == "Shopping" || activity.type[1] == "Shopping");
+let shopResult = actList.filter(activity => activity.type[0] == "Shopping" || activity.type[1] == "Shopping");
 
-console.log("shopResult", shopResult) // Working
 
-buildTable(shopResult) // not working
+buildTable(shopResult) 
 
 })
 
-// Social
 
 
 socialLink.addEventListener("click", (event) => {
 console.log("socialLink", socialLink)
-let socialResult = actList.filter(activity => activity.type[0] == "Social" || activity.type[1] == "Social");
+let shopResult = actList.filter(activity => activity.type[0] == "Social" || activity.type[1] == "Social");
 
-console.log("socialResult", socialResult) // Working
 
-buildTable(socialResult) // not working
+buildTable(shopResult) 
 
 })
 
-// Variable
+
 
 
 varLink.addEventListener("click", (event) => {
 console.log("varLink", varLink)
 let varResult = actList.filter(activity => activity.price[0] == "Variable" || activity.price[1] == "Variable" || activity.price[2] == "Variable");
 
-console.log("varResult", varResult) // Working
 
-buildTable(varResult) // not working
+buildTable(varResult) 
 
 })
 
-// Free
+
 
 freeLink.addEventListener("click", (event) => {
 console.log("freeLink", freeLink)
 let freeResult = actList.filter(activity => activity.price[0] == "Free" || activity.price[1] == "Free" || activity.price[2]== "Free");
 
-console.log("freeResult", freeResult) // Working
+console.log("freeResult", freeResult) 
 
-buildTable(freeResult) // not working
-
+buildTable(freeResult) 
 })
 
-// Low
 
 lowLink.addEventListener("click", (event) => {
 console.log("lowLink", lowLink)
 let lowResult = actList.filter(activity => activity.price[0] == "Low" || activity.price[1] == "Low");
 
-console.log("lowResult", lowResult) // Working
+console.log("lowResult", lowResult) 
 
-buildTable(lowResult) // not working
+buildTable(lowResult) 
 
 })
 
-// Medium
+
 
 medLink.addEventListener("click", (event) => {
-console.log("Link", Link)
+
 let medResult = actList.filter(activity => activity.price[0] == "Medium" || activity.price[1] == "Medium" || activity.price[2]== "Medium");
 
-console.log("medResult", medResult) // Working
 
-buildTable(medResult) // not working
+
+buildTable(medResult) 
 
 })
 
-// Expensive
+
 
 expLink.addEventListener("click", (event) => {
 console.log("expLink", expLink)
 let expResult = actList.filter(activity => activity.price[0] == "Expensive" || activity.price[1] == "Expensive" || activity.price[2] == "Expensive" || activity.price[3] == "Expensive");
 
-console.log("expResult", expResult) // Working
 
-buildTable(expResult) // not working
-
-})
-
-
-})
+buildTable(expResult) 
 
 })
 
 
+})
 
-
-
-
-
-
-
-
-
-
- 
-
-
-
-    
-
-
-
-
-
-
-
-
-// // //All art activities :D
-
-// //     let result = actList.filter(activity => activity.type == "Art");
-// //     console.log(result)
+})
